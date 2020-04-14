e4558d0a7f7b5959cbac4889f074286f6b2b7cb7
michelangelo17 <michelangelo.markus@gmail.com>
2020-04-14 14:19:16 -0700

    fixed stock price

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,18 +6,31 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 
+# dictionary works because when function first called,
+# points to same slot in memory from then on,
+# and dictionary is mutable, allowing storage to be passed on
+
 
 def eating_cookies(n, cache={}):
+    # track number of permutations
     count = 0
+    # check if n result is in dictionary
     if n in cache:
         return cache[n]
+    # if n is less than zero return 0
+    # count will remain the same on recursive calls
     if n < 0:
         return 0
+    # each zero case is 1 combination
     if n == 0:
         return 1
+    # run each n through each possiblity recursively
+    # ex: 3 - 3 count 1, 3 - 2, -> 1 - 1, count 2, 3 - 1, 2 -2, count 3 etc.
+    # negative values return 0 as above
     count += eating_cookies(n - 3)
     count += eating_cookies(n - 2)
     count += eating_cookies(n - 1)
+    # store each number's count in dictionary
     cache[n] = count
     return count
 
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,40 +4,31 @@ import argparse
 
 
 def find_max_profit(prices):
-    # copy list in case max is at index 0
-    prices_copy = prices[:]
-
     # find min and max values and index
-    min_price = min(prices_copy)
-    max_price = max(prices_copy)
-    min_index = prices_copy.index(min_price)
-    max_index = prices_copy.index(max_price)
+    min_price = min(prices)
+    max_price = max(prices)
+    min_index = prices.index(min_price)
+    max_index = prices.index(max_price)
 
     # optimisation in case min and max are arranged ideally
     if min_index < max_index:
         return max_price - min_price
-
-    # remove max from beginning of array to resolve edge case
-    # example: 1000, 100, 10 would result in -900 without this
-    while max_index == 0 and len(prices_copy) > 2:
-        prices_copy.pop(0)
-        max_price = max(prices_copy)
-        max_index = prices_copy.index(max_price)
-
     else:
-        # set default values to first array pair to handle negative responses
-        best_with_max = prices_copy[1] - prices_copy[0]
-        best_with_min = prices_copy[1] - prices_copy[0]
+        # set default values to first and last array pairs
+        # this way, negative values handled correctly
+        best_with_max = prices[1] - prices[0]
+        best_with_min = prices[len(
+            prices) - 1] - prices[len(prices) - 2]
 
         # test with values to the left of max index
         for i in range(1, max_index):
-            test_with_max = max_price - prices_copy[i]
+            test_with_max = max_price - prices[i]
             if test_with_max > best_with_max:
                 best_with_max = test_with_max
 
         # test with values to the right of min_index
-        for j in range(min_index + 1, len(prices_copy)):
-            test_with_min = prices_copy[j] - min_price
+        for j in range(min_index + 1, len(prices) - 1):
+            test_with_min = prices[j] - min_price
             if test_with_min > best_with_min:
                 best_with_min = test_with_min
 
