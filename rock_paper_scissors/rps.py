cccb9d3978097b54adc0018202041e643a74e80f
michelangelo17 <michelangelo.markus@gmail.com>
2020-04-14 22:30:16 -0700

    solved rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -4,7 +4,20 @@ import sys
 
 
 def rock_paper_scissors(n):
-    pass
+    # create nested lists times amount
+    # ex: n = 2 results -> [['rock', 'paper', 'scissors'], ['rock', 'paper', 'scissors']]
+    moves_lists = [['rock', 'paper', 'scissors']] * n
+    result = [[]]
+    # for each array in nested moves array
+    for moves in moves_lists:
+        # list comprehension to split list using nested for loop and combine on left
+        # result for y first pass creates 'rock', 'paper' 'scissors'
+        # x for first pass creates []
+        # [] + ['rock'] = [['rock]]
+        # then result for second pass is [['rock], ['paper], ['scissors']]
+        # x = ['rock'] + [y='rock'] giving us [['rock', 'rock']]
+        result = [x+[y] for x in result for y in moves]
+    return result
 
 
 if __name__ == "__main__":
