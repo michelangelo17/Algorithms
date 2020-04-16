3bf0c9483b105424751e6c9992c3b1a3af4e9ac2
michelangelo17 <michelangelo.markus@gmail.com>
2020-04-15 20:45:07 -0700

    solved making_change, with increased recursion depth, objections noted and disregarded

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -1,35 +1,58 @@
 #!/usr/bin/python
 
 import sys
+# i know this is 'not advised', but this is a good, fast solution so i'm doing it anyway
+sys.setrecursionlimit(10**5)
 
 
-def making_change(amount, denominations):
-    # track number of permutations
+def making_change(amount, denominations, cache={1: {}, 5: {}, 10: {}, 25: {}, 50: {}}):
+    # in below loop, the beginning of the list is changed as it goes through
+    # sets current_coint to beggining of that array to use with cache dictionary
+    current_coin = denominations[0]
+    # checks cache for amount with list beggining with current coin
+    if amount in cache[current_coin]:
+        return cache[current_coin][amount]
+    # holds permutation count
     count = 0
-    # if n is less than zero return 0
-    # count will remain the same on recursive calls
+    # base case is to exactly hit one, means coin combination subtracted down evenly
     if amount < 0:
         return 0
-    # each zero case is 1 combination
     if amount == 0:
         return 1
-    # run each n through each possiblity recursively
-    # ex: 3 - 3 count 1, 3 - 2, -> 1 - 1, count 2, 3 - 1, 2 -2, count 3 etc.
-    # negative values return 0 as above
-    for coin in denominations:
-        count += making_change(amount - coin, denominations)
+    # loop over denominations array, checking each coin recursively
+    # uses index to prevent repeating coin combinations.
+    # ex: 6 -> 1, 5 when current_coin is 1, then when current coin is 5
+    # there will be no 1 to combine with, so will not get a reversed 5, 1 double count
+    for i, coin in enumerate(denominations):
+        count += making_change(amount - coin, denominations[i:])
+    # add new amount values for current coin dictionary in cache
+    cache[current_coin][amount] = count
     return count
 
 
-print(making_change(6, [1, 5, 10, 25, 50]))
+print(making_change(10000, [1, 5, 10, 25, 50]))
 
-# if __name__ == "__main__":
-#     # Test our your implementation from the command line
-#     # with `python making_change.py [amount]` with different amounts
-#     if len(sys.argv) > 1:
-#         denominations = [1, 5, 10, 25, 50]
-#         amount = int(sys.argv[1])
-#         print("There are {ways} ways to make {amount} cents.".format(
-#             ways=making_change(amount, denominations), amount=amount))
-#     else:
-#         print("Usage: making_change.py [amount]")
+if __name__ == "__main__":
+    # Test our your implementation from the command line
+    # with `python making_change.py [amount]` with different amounts
+    if len(sys.argv) > 1:
+        denominations = [1, 5, 10, 25, 50]
+        amount = int(sys.argv[1])
+        print("There are {ways} ways to make {amount} cents.".format(
+            ways=making_change(amount, denominations), amount=amount))
+    else:
+        print("Usage: making_change.py [amount]")
+
+# would be nice, but recursion depth exceeds limits
+# def making_change(amount, denominations, cache={1: {}, 5: {}, 10: {}, 25: {}, 50: {}}):
+#     if amount in cache[denominations[0]]:
+#         return cache[denominations[0]][amount]
+#     count = 0
+#     if amount < 0:
+#         return 0
+#     if amount == 0:
+#         return 1
+#     for i, coin in enumerate(denominations):
+#         count += making_change(amount - coin, denominations[i:])
+#     cache[denominations[0]][amount] = count
+#     return count
